10fb26c795975c64fd7f77575253f78f5ac842b2
MarcelloClaud <marcello.bez2001@gmail.com>
2022-08-09 14:25:46 +0200

    Added Controllers

--- 34494847-API/34494847-API/ConnectedOfficeContext.cs
+++ 34494847-API/34494847-API/ConnectedOfficeContext.cs
@@ -23,11 +23,7 @@ namespace _34494847_API
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                //To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("dbcon");
-            }
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
